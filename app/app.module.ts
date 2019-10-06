e5ed3a650590c9b2cb8dd1f8705de192e80256af
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-10-06 22:24:47 +0530

    Assignment - Submision

---
+++ app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { CollapseModule } from "ngx-bootstrap/collapse";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { JobListingComponent } from "./job-listing/job-listing.component";
+
+import { Ng2SmartTableModule } from "ng2-smart-table";
+import { DataTableModule } from "angular2-datatable";
+import { ContractorsComponent } from "./contractors/contractors.component";
+import { WorkersComponent } from "./workers/workers.component";
+import { ComingSoonComponent } from "./coming-soon/coming-soon.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AddWorkerComponent } from "./workers/add-worker/add-worker.component";
+import { FormsModule } from "@angular/forms";
+
+import { ReactiveFormsModule } from "@angular/forms";
+import { LoaderComponent } from './ui/loader/loader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    JobListingComponent,
+    ContractorsComponent,
+    WorkersComponent,
+    ComingSoonComponent,
+    AddWorkerComponent,
+    LoaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    CollapseModule.forRoot(),
+    AppRoutingModule,
+    Ng2SmartTableModule,
+    DataTableModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
