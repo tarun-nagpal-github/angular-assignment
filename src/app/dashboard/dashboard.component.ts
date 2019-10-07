687d32d823f3567d99cd482abf847c7f985281ce
Tarun Nagpal <tarun.nagpal78@gmail.com>
2019-10-07 06:05:26 +0530

    Added un Tracked Files

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
+})
+export class DashboardComponent {}
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Observable, Subscribable } from "rxjs";
+// import  { Headers, RequestOptions}  from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ApiService {
+  // apiURL: string = "http://localhost/angular-project/";
+  xapiURL: string = "http://api.ngaituk.online/";
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json"
+    }),
+    responseType: "text" as "json"
+  };
+  constructor(private httpClient: HttpClient) { }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
+
+  getWorkers(id = null): Observable<any> {
+    return this.httpClient.get(`${this.apiURL}/workers/getWorkers.php`);
+  }
+
+  deleteWorker(id = null): Observable<any> {
+    // let httpParams = new HttpParams();
+    // httpParams.set("id", id);
+    // let options = { params: httpParams };
+
+    // http://localhost/angular-project/workers/deleteWorker.php?id=109
+    return this.httpClient.delete(
+      `${this.apiURL}/workers/deleteWorker.php?id=${id}`,
+      {}
+    );
+  }
+
+  addWorker(body): Observable<any> {
+    return this.httpClient.post(
+      `${this.apiURL}/workers/createWorker.php`,
+      body,
+      this.httpOptions
+    );
+  }
+}
